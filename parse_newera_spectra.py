a7c12b137cf2d6e51de99ee98a86090163429796
SteveOv <steveoverall@hotmail.co.uk>
2025-05-21 17:50:49 +0100

    initial go at using NewEra spectra for the mcmc fitting

--- parse_newera_spectra.py
+++ parse_newera_spectra.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ Script to parse previously downloaded full Z=0 spectra files from the NewEra archive. """
+# pylint: disable=no-member
 from pathlib import Path
+import pickle as pkl
 
 import numpy as np
 from scipy.interpolate import interp1d
-import pickle as pkl
+import astropy.units as u
 
 # Specific requirement for these files
 import h5py
@@ -23,7 +25,7 @@ print(f"Found {spec_count} NewEra h5 files matching the glob '{FILE_PATTERN}' in
 fields_index = { "teff": "teff", "logg": "logg", "mass": "m_sun" }
 fields_dtype = [("teff", float), ("logg", float), ("mass", float),
                 ("lam_from", float), ("lam_to", float), ("lam_steps", int)]
-bandpass = (0.25, 20.0) # um
+bandpass = (0.25, 20.0) * u.micron
 table = np.empty((spec_count, ), dtype=fields_dtype + [("flux_interp", object)])
 
 mask_sl = slice(None, None, 10)
@@ -31,25 +33,26 @@ mask_sl = slice(None, None, 10)
 for spec_ix, spec_file in enumerate(spec_files):
     print(f"Parsing file {spec_ix+1} of {spec_count}: {spec_file.name}", end="")
     with h5py.File(spec_file) as sf:
+        # wavelents stored as Angstroem in vacuum, fluxes as log10(F_lambda [erg/cm^2/s/cm])
+        wl = sf["/PHOENIX_SPECTRUM_LSR/wl"][()] * u.Angstrom
+        fl = 10.**sf["/PHOENIX_SPECTRUM_LSR/fl"][()] / 1e8 * u.erg / (u.cm**2 * u.s * u.Angstrom)
 
-        lams = sf["/PHOENIX_SPECTRUM_LSR/wl"][()] / 1e4     # Angstroem to um
-        fluxes = 10.**sf["/PHOENIX_SPECTRUM_LSR/fl"][()]    # log10 to linear
-
-        nml = f90nml.reads(str(sf['/PHOENIX_NAMELIST/phoenix_nml'][()].tobytes())[2:-1]) # pylint: disable=no-member
+        # Get the flux F(nu) and then into SI units
+        fnu = 3.34e-19 * wl.value**2 * fl.value * u.erg / (u.cm**2 * u.s * u.Hz)
+        fnu = fnu.to(u.W / (u.m**2 * u.Hz)).value
 
         # Create the row, with an interp1d for the freq to flux
-        mask = (lams > min(bandpass)) & (lams < max(bandpass))
-        lams = lams[mask][mask_sl]
-        table[spec_ix]["flux_interp"] = interp1d(c*1e6 / lams, fluxes[mask][mask_sl], kind="linear")
-        table[spec_ix]["lam_from"] = lams.min()
-        table[spec_ix]["lam_to"] = lams.max()
-        table[spec_ix]["lam_steps"] = len(lams)
+        mask = (wl > min(bandpass)) & (wl < max(bandpass))
+        wl = (wl[mask][mask_sl]).to(u.micron).value
+        table[spec_ix]["flux_interp"] = interp1d(c*1e6 / wl, fnu[mask][mask_sl], kind="linear")
+        table[spec_ix]["lam_from"] = wl.min()
+        table[spec_ix]["lam_to"] = wl.max()
+        table[spec_ix]["lam_steps"] = len(wl)
+
+        nml = f90nml.reads(str(sf['/PHOENIX_NAMELIST/phoenix_nml'][()].tobytes())[2:-1])
         for field, nml_key in fields_index.items():
             table[spec_ix][field] = nml["phoenix"][nml_key]
-        print(f"...stored flux interp at {len(lams)} frequencies")
+        print(f"...stored flux interp at {len(wl)} frequencies")
 
-#
-save_file = Path("libs/data/newera/PHOENIX-NewEra-LR.Z-0.0.pkl")
-np.save(save_file.parent / (save_file.stem + ".npy"), table, allow_pickle=True)
-with open(save_file, "wb") as sf:
-    pkl.dump(table, sf)
+# Save with interps serialized
+np.save("libs/data/newera/PHOENIX-NewEra-LR.Z-0.0.npy", table, allow_pickle=True)
