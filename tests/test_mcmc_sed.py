f63c6976008837ac9e5ca089c1abe627ae10bfe8
SteveOv <steveoverall@hotmail.co.uk>
2025-06-05 11:45:25 +0100

    exteded minimize test to NewEra model and SLSQP method
    TBH it doesn't look good - your starting position needs to be close to get a useful answer

--- tests/test_mcmc_sed.py
+++ tests/test_mcmc_sed.py
@@ -5,7 +5,10 @@ import unittest
 import numpy as np
 import astropy.units as u
 
-from scipy.optimize import minimize, show_options
+from scipy.optimize import minimize
+from scipy.interpolate import RegularGridInterpolator
+
+from deblib.constants import c
 
 from libs.pipeline import get_sed_for_target
 from libs.mcmc_sed import norm_blackbody_model, min_max_normalize
@@ -18,33 +21,46 @@ class Testmcmcsed(unittest.TestCase):
     def test_scipy_optimize_norm_bb_model(self):
         """ Try scipy.minimize to see if it's an option for initial estimates. """
         # pylint: disable=too-many-locals, unused-variable
-        # Ratios from LC fitting
-        target = "CM Dra"
-        k = 0.945                       # Fitted
-        J = 0.98133                     # Fitted
-        Teff_tess = 3214.00             # TESS metadata
+        target = "CW Eri"
+        J = 0.93            # From LC fit
+        Teff_tess = 6900    # From TESS metadata (6861) rounded to 2 s.f.
 
-        # Get some SED data (overlook extinction)
+        # Set up the NewEra model function
+        spec = np.load("libs/data/newera/PHOENIX-NewEra-for-interp3d.npy.npz", allow_pickle=True)
+        ne_wl_range = c * 1e6 / (spec["freqs"].max(), spec["freqs"].min()) * u.um
+        interp = RegularGridInterpolator(points=(spec["teffs"], spec["loggs"], spec["freqs"]), values=spec["V"])
+
+        def newera_model(x, tA, tB, lA, lB):
+            return min_max_normalize(np.add(interp(np.array([[tA, lA, freq] for freq in x]), method="linear"),
+                                            interp(np.array([[tB, lB, freq] for freq in x]), method="linear")))
+
+        # Get target's SED data (overlook extinction). Restrict bandpass to within the NewEra data
         sed = get_sed_for_target(target, f"V* {target}", 0.1)
-        mask = (sed["sed_wl"] > 0.4*u.um) & (sed["sed_wl"] < 20*u.um) & (sed["sed_filter"] != "HIP:hp")
+        mask = (sed["sed_wl"] > min(ne_wl_range)) & (sed["sed_wl"] < max(ne_wl_range)) \
+            & ([not f.startswith("HIP:") for f in sed["sed_filter"]]) & (sed["sed_filter"] != "Cousins:I")
         sed_freq = sed[mask]["sed_freq"].to(u.Hz).value
         sed_norm_flux, sed_norm_eflux = min_max_normalize(sed[mask]["sed_flux"].value, sed[mask]["sed_eflux"].value * 5)
 
-        def similarity_func(theta, x, y, y_err):
-            (Teff1, logg1, logg2) = theta
-            y_model = norm_blackbody_model(x, Teff1, Teff1 * J, logg1, logg2)
+        for msg, model_func in [("Blackbody", norm_blackbody_model), ("NewEra", newera_model)]:
+            for method in ["Nelder-Mead", "SLSQP"]:
+                soln = minimize(Testmcmcsed._similarity_func,
+                                x0=np.array([Teff_tess, 5, 5]), # TeffA, loggA, loggB
+                                args=(sed_freq, sed_norm_flux, sed_norm_eflux, J, model_func),
+                                method=method, options={"disp": True})
+                (TeffA, loggA, loggB), TeffB = soln.x, soln.x[0] * J
+                print(f"\nThe {msg} model minimized with the {method} method yielded the following for {target}\n{soln}")
+                print(f"TeffA={TeffA:.0f} K & loggA={loggA:.3f} dex; TeffB={TeffB:.0f} K & loggB={loggB:.3f}")
+
+
+    @classmethod
+    def _similarity_func(cls, theta, x, y, y_err, J, model_func: norm_blackbody_model):
+        """ Simple function for comparing model SEDs with obs when called from scipy.minimize"""
+        # pylint: disable=too-many-arguments, too-many-positional-arguments
+        (tA, lA, lB), tB = theta, theta[0] * J
+        if 2300 < tA < 12000 and 2300 < tB < 12000 and 0.5 < lA < 6.0 and 0.5 < lB < 6.0: # priors
+            y_model = model_func(x, tA, tB, lA, lB)
             return 0.5 * np.sum(((y - y_model) / y_err)**2)
-
-        x0 = np.array([Teff_tess, 4, 4]) # Teff1|A, logg1|A, logg2|A
-        method="Nelder-Mead"
-        soln = minimize(similarity_func, x0, args=(sed_freq, sed_norm_flux, sed_norm_eflux),
-                        method=method, options={"disp": False})
-
-        (TeffA, loggA, loggB), TeffB = soln.x, soln.x[0] * J
-        print()
-        #show_options("minimize", method, disp=True)
-        print(soln)
-        print(f"TeffA={TeffA:.0f} K & loggA={loggA:.3f} dex; TeffB={TeffB:.0f} K & loggB={loggB:.3f}")
+        return np.inf
 
 if __name__ == "__main__":
     unittest.main()
