a7b84bbfea9123590a622069c0e5172de098cdf2
SteveOv <steveoverall@hotmail.co.uk>
2025-06-06 17:29:50 +0100

    fixed test following removal of bb_model from mcmc_sed module

--- tests/test_mcmc_sed.py
+++ tests/test_mcmc_sed.py
@@ -8,16 +8,17 @@ import astropy.units as u
 from scipy.optimize import minimize
 from scipy.interpolate import RegularGridInterpolator
 
-from deblib.constants import c
+from deblib.constants import c, h, k_B
+from deblib.vmath import exp
 
 from libs.pipeline import get_sed_for_target
-from libs.mcmc_sed import norm_blackbody_model, min_max_normalize
+from libs.mcmc_sed import min_max_normalize
 
 
 class Testmcmcsed(unittest.TestCase):
     """ Unit tests for the mcmc_sed module. """
 
-    @unittest.skip("not really a test as such - more speculative behaviour")
+    #@unittest.skip("not really a test as such - more speculative behaviour")
     def test_scipy_optimize_norm_bb_model(self):
         """ Try scipy.minimize to see if it's an option for initial estimates. """
         # pylint: disable=too-many-locals, unused-variable
@@ -34,6 +35,12 @@ class Testmcmcsed(unittest.TestCase):
             return min_max_normalize(np.add(interp(np.array([[tA, lA, freq] for freq in x]), method="linear"),
                                             interp(np.array([[tB, lB, freq] for freq in x]), method="linear")))
 
+        def bb_model(x, tA, tB, lA, lB):
+            # pylint: disable=unused-argument
+            def bb_spectrum(freqs, teff):
+                return ((2 * h * freqs**3) / c**2) / (exp((h * freqs) / (k_B * teff)) - 1)
+            return min_max_normalize(np.add(bb_spectrum(x, tA), bb_spectrum(x, tB)))
+
         # Get target's SED data (overlook extinction). Restrict bandpass to within the NewEra data
         sed = get_sed_for_target(target, f"V* {target}", 0.1)
         mask = (sed["sed_wl"] > min(ne_wl_range)) & (sed["sed_wl"] < max(ne_wl_range)) \
@@ -41,7 +48,7 @@ class Testmcmcsed(unittest.TestCase):
         sed_freq = sed[mask]["sed_freq"].to(u.Hz).value
         sed_norm_flux, sed_norm_eflux = min_max_normalize(sed[mask]["sed_flux"].value, sed[mask]["sed_eflux"].value * 5)
 
-        for msg, model_func in [("Blackbody", norm_blackbody_model), ("NewEra", newera_model)]:
+        for msg, model_func in [("Blackbody", bb_model), ("NewEra", newera_model)]:
             for method in ["Nelder-Mead", "SLSQP"]:
                 soln = minimize(Testmcmcsed._similarity_func,
                                 x0=np.array([Teff_tess, 5, 5]), # TeffA, loggA, loggB
@@ -53,7 +60,7 @@ class Testmcmcsed(unittest.TestCase):
 
 
     @classmethod
-    def _similarity_func(cls, theta, x, y, y_err, J, model_func: norm_blackbody_model):
+    def _similarity_func(cls, theta, x, y, y_err, J, model_func):
         """ Simple function for comparing model SEDs with obs when called from scipy.minimize"""
         # pylint: disable=too-many-arguments, too-many-positional-arguments
         (tA, lA, lB), tB = theta, theta[0] * J
