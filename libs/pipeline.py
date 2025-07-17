08a01ffcb642620c6940c1ff4c5bc2fa62c8457a
SteveOv <steveoverall@hotmail.co.uk>
2025-07-17 14:00:41 +0100

    moved the get_teff_from_spt() func into pipeline

--- libs/pipeline.py
+++ libs/pipeline.py
@@ -3,8 +3,9 @@ Low level utility functions for light curve ingest, pre-processing, estimation a
 """
 #pylint: disable=no-member
 from typing import Tuple
-from requests.exceptions import HTTPError
+import re
 
+from requests.exceptions import HTTPError
 import numpy as np
 from uncertainties import UFloat, ufloat
 import astropy.units as u
@@ -17,6 +18,16 @@ from deblib.vmath import arccos, arcsin, degrees
 
 _TRIG_MIN = ufloat(-1, 0)
 _TRIG_MAX = ufloat(1, 0)
+
+_spt_to_teff_map = {
+    "M": ufloat(3100, 800),
+    "K": ufloat(4600, 700),
+    "G": ufloat(5650, 350),
+    "F": ufloat(6700, 500),
+    "A": ufloat(8600, 1300),
+    "B": ufloat(20000, 10000),
+    "O": ufloat(35000, 10000)
+}
 
 def append_calculated_inc_predictions(preds: np.ndarray[UFloat],
                                       field_name: str="inc") -> np.ndarray[UFloat]:
@@ -55,61 +66,19 @@ def append_calculated_inc_predictions(preds: np.ndarray[UFloat],
     return new
 
 
-def get_bayestar_ebv_for_coords(target_coords: SkyCoord,
-                                version: str="bayestar2019",
-                                ebv_factor: float=0.996) -> Tuple[float, dict]:
+def get_teff_from_spt(target_spt):
     """
-    Queries the Bayestar 2019 dereddening map for the E(B-V) value for the target coordinates.
+    Estimates a stellar T_eff [K] from the passed spectral type.
 
-    Conversion from Bayestar 17 or 19 to E(B-V) documented at http://argonaut.skymaps.info/usage
-    as E(B-V) = 0.884 x bayestar or E(B-V) = 0.996 x bayestar
-    
-    :target_coords: the astropy SkyCoords to query for
-    :version: the version of the Bayestar dust maps to use
-    :ebv_factor: the conversion factor from bayestar extinction to E(B-V) to use
-    :returns: tuple of the E(B-V) value and a dict of the diagnostic flags associated with the query
+    :target_spt: the spectral type string
+    :returns: the estimated teff in K
     """
-    try:
-        # Creates/confirms local cache of Bayestar data within the .cache directory
-        config.config['data_dir'] = '.cache/.dustmapsrc'
-        bayestar.fetch(version=version)
-    except HTTPError as exc:
-        print(f"Unable to (re)fetch data for {version}. Caught error '{exc}'")
-    except ValueError as exc:
-        print(f"Unable to parse response for {version}. Caught error '{exc}'")
+    teff = None
 
-    # Now we can use the local cache for the lookup
-    query = bayestar.BayestarQuery(version=version)
-    ebv, flags =  query(target_coords, mode='median', return_flags=True)
-    return ebv_factor * ebv, { n: flags[n] for n in flags.dtype.names }
-
-
-def get_vergely_av_for_coords(target_coords: SkyCoord):
-    """
-    Queries the Vergely, Lallement & Cox (2022) 3-d extinction map for the Av value of the
-    target coordinates.
-
-    :target_coords: the astropy SkyCoords to query for   
-    :returns: the Av value
-    """
-    av = None
-    try:
-        # Extinction map of Vergely, Lallement & Cox (2022)
-        ivoid = 'ivo://CDS.VizieR/J/A+A/664/A174'
-        table = 'J/A+A/664/A174/cube_ext'
-        vo_res = registry.search(ivoid=ivoid)[0]
-        print(f"Querying {vo_res.res_title} ({vo_res.source_value}) for extinction data.")
-
-        for res in [10, 50]: # central regions at 10 pc resolution and outer at 50 pc
-            cart = np.round(target_coords.cartesian.xyz.to(u.pc) / res) * res
-            rec = vo_res.get_service('tap').search(f'SELECT * FROM "{table}" ' +
-                f'WHERE x={cart[0].value:.0f} AND y={cart[1].value:.0f} AND z={cart[2].value:.0f}')
-
-            if len(rec):
-                # TODO: anything extra to map these values to Av?
-                ext_nmag_per_pc = rec["Exti"][0] # nmag
-                av = (ext_nmag_per_pc * target_coords.distance.to(u.pc).value) / 10**9
-                break
-    except DALServiceError as exc:
-        print(f"Failed to query: {exc}")
-    return av
+    # Also add the whole spt in case it's just a single char (i.e.: V889 Aql is set to "A")
+    if (spts := re.findall(r"([A-Z][0-9])", target_spt) + [target_spt.upper()]) is not None:
+        for spt in spts:
+            if (tp := spt.strip()[0]) in _spt_to_teff_map \
+                and _spt_to_teff_map[tp].n > (teff.n if teff is not None else 0):
+                teff = _spt_to_teff_map[tp]
+    return teff
