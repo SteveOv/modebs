cee87eeb2d26cafa1f815541a0e046c4a16c7792
SteveOv <steveoverall@hotmail.co.uk>
2025-07-18 12:50:40 +0100

    append_calc_inc... catch uncertainties' FutureWarnings (resolves #7)

--- libs/pipeline.py
+++ libs/pipeline.py
@@ -2,17 +2,11 @@
 Low level utility functions for light curve ingest, pre-processing, estimation and fitting.
 """
 #pylint: disable=no-member
-from typing import Tuple
+import warnings
 import re
 
-from requests.exceptions import HTTPError
 import numpy as np
 from uncertainties import UFloat, ufloat
-import astropy.units as u
-from astropy.coordinates import SkyCoord
-
-from dustmaps import config, bayestar           # Bayestar dustmaps/dereddening map
-from pyvo import registry, DALServiceError      # Vergeley at al. extinction catalogue
 
 from deblib.vmath import arccos, arcsin, degrees
 
@@ -38,32 +32,35 @@ def append_calculated_inc_predictions(preds: np.ndarray[UFloat],
     :field_name: the name of the inclination field to write to
     :returns: the revised array
     """
-    names = list(preds.dtype.names)
-    if "bP" in names:
-        # From primary impact param:  i = arccos(bP * r1 * (1+esinw)/(1-e^2))
-        r1 = preds["rA_plus_rB"] / (1+preds["k"])
-        e_squared = preds["ecosw"]**2 + preds["esinw"]**2
-        cosi = np.clip(preds["bP"] * r1 * (1+preds["esinw"]) / (1-e_squared), _TRIG_MIN, _TRIG_MAX)
-        inc = degrees(arccos(cosi))
-    elif "cosi" in names:
-        cosi = np.clip(preds["cosi"], _TRIG_MIN, _TRIG_MAX)
-        inc = degrees(arccos(cosi))
-    elif "sini" in names:
-        sini = np.clip(preds["sini"], _TRIG_MIN, _TRIG_MAX)
-        inc = degrees(arcsin(sini))
-    else:
-        raise KeyError("Missing bP, cosi or sini in predictions required to calc inc.")
+    with warnings.catch_warnings(category=[FutureWarning]):
+        # Deprecation warning caused by the use of np.clip on ufloats
+        warnings.filterwarnings("ignore", r"AffineScalarFunc.(__le__|__ge__)\(\) is deprecated.")
+        names = list(preds.dtype.names)
+        if "bP" in names:
+            # From primary impact param:  i = arccos(bP * r1 * (1+esinw)/(1-e^2))
+            r1 = preds["rA_plus_rB"] / (1+preds["k"])
+            e_squared = preds["ecosw"]**2 + preds["esinw"]**2
+            cosi = np.clip(preds["bP"]*r1*(1+preds["esinw"]) / (1-e_squared), _TRIG_MIN, _TRIG_MAX)
+            inc = degrees(arccos(cosi))
+        elif "cosi" in names:
+            cosi = np.clip(preds["cosi"], _TRIG_MIN, _TRIG_MAX)
+            inc = degrees(arccos(cosi))
+        elif "sini" in names:
+            sini = np.clip(preds["sini"], _TRIG_MIN, _TRIG_MAX)
+            inc = degrees(arcsin(sini))
+        else:
+            raise KeyError("Missing bP, cosi or sini in predictions required to calc inc.")
 
-    if field_name not in names:
-        # It's difficult to append a field to structured array or recarray so copy over to new inst.
-        # The numpy recfunctions module has merge and append_field funcs but they're slower.
-        new = np.empty_like(preds, np.dtype(preds.dtype.descr + [(field_name, UFloat.dtype)]))
-        new[names] = preds[names]
+        if field_name not in names:
+            # It's difficult to append a field to structured array or recarray so copy to new inst.
+            # The numpy recfunctions module has merge and append_field funcs but they're slower.
+            new = np.empty_like(preds, np.dtype(preds.dtype.descr + [(field_name, UFloat.dtype)]))
+            new[names] = preds[names]
+            new[field_name] = inc
+        else:
+            new = preds
         new[field_name] = inc
-    else:
-        new = preds
-    new[field_name] = inc
-    return new
+        return new
 
 
 def get_teff_from_spt(target_spt):
