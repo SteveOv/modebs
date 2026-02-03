b5838af0d68488090bcdcdddabd38c28f8a8c625
SteveOv <steveoverall@hotmail.co.uk>
2026-02-03 18:04:46 +0000

    added lookup of J+A2021 ebs catalogue

--- libs/catalogues.py
+++ libs/catalogues.py
@@ -11,9 +11,9 @@ from astropy.table import Table
 # Used to read the number from TIC ##### format tics
 _tic_num_pattern = re.compile(r"TIC\s*(?P<tic>\d+)", re.IGNORECASE)
 
-def query_tess_ebs(tics: List[Union[int, str]]) -> Dict[str, Union[float, UFloat]]:
+def query_tess_ebs_ephemeris(tics: List[Union[int, str]]) -> Dict[str, Union[float, UFloat]]:
     """
-    Gets a dictionary of ephemeris and morphology data from the TESS-ebs catalogue.
+    Gets a dictionary of ephemeris and morphology data from the TESS-ebs catalogue (J/ApJS/258/16).
     Takes a list of TIC ids and returns the data for the first match, or None if no match.
 
     Previously we queried TESS-ebs with astroquery.Vizier which is more flexible at resolving Ids,
@@ -29,19 +29,8 @@ def query_tess_ebs(tics: List[Union[int, str]]) -> Dict[str, Union[float, UFloat
                        readme=Path("./libs/data/tess-ebs/ReadMe"),
                        format="ascii.cds")
 
-    if isinstance(tics, Number|str):
-        tics = [tics]
-
-    for tic in tics:
-        if isinstance(tic, str):
-            if tic.isnumeric():
-                tic = int(tic)
-            elif (match := _tic_num_pattern.match(tic)) is not None and "tic" in match.groupdict():
-                tic = int(match.groups("tic"))
-            else:
-                tic = None
-
-        if tic is not None and any(tic_mask := table["TIC"] == tic):
+    for tic in _yield_tic_nums(tics):
+        if any(tic_mask := table["TIC"] == tic):
             row = table[tic_mask][0]
 
             period = ufloat(row["Per"], row["e_Per"])
@@ -89,5 +78,60 @@ def query_tess_ebs(tics: List[Union[int, str]]) -> Dict[str, Union[float, UFloat
 
             # We have a match to a TIC number
             break
-
     return data
+
+
+def query_tess_ebs_in_sh(tics: List[Union[int, str]]) -> dict:
+    """
+    Gets a dictionary of ephemeris and physical data from table 3 of the TESS EBs in the
+    southern hemisphere catalogue of Justesen & Albrecht (2021) (J/ApJ/912/123).
+    Takes a list of TIC ids and returns the data for the first match, or None if no match.
+
+    :tics: the potential ids for the target (some may have more than one TIC)
+    :returns: dict of the requested data
+    """
+    # pylint: disable=too-many-locals
+
+    # May move to set this up just once (LRU cache?)
+    table = Table.read(Path("./libs/data/catalogues/J_ApJ_912_123/table3.dat"),
+                       readme=Path("./libs/data/catalogues/J_ApJ_912_123/ReadMe"),
+                       format="ascii.cds")
+
+    data = None
+    for tic in _yield_tic_nums(tics):
+        if any(tic_mask := table["TIC"] == tic):
+            row = table[tic_mask][0]
+
+            data = {
+                "t0": row["t1"],
+                "period": row["Per"],
+                "k": row["rp"],
+                "a/R1": row["a/R1"],
+                "ecosw": row["ecosw"],
+                "esinw": row["esinw"],
+                "inc": row["inc"],
+                "light_ratio": row["fp"],
+                "TeffA": row["Teff1"],
+                "TeffB": row["Teff2"]
+            }
+
+            # We have a match to a TIC number
+            break
+    return data
+
+
+def _yield_tic_nums(tics: List[Union[int, str]]):
+    """
+    Parse a list of putative TIC identifiers and yield the numeric component of those that are valid
+    """
+    if isinstance(tics, str|Number):
+        tics = [tics]
+
+    for tic in tics:
+        if isinstance(tic, Number):
+            yield int(tic)
+        elif isinstance(tic, str):
+            if tic.isnumeric():
+                yield int(tic)
+            elif (match := _tic_num_pattern.match(tic)) is not None and "tic" in match.groupdict():
+                yield int(match.group("tic"))
