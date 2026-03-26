a182c1d50116cb23744570ecc4f65d3d64f3d7e5
SteveOv <steveoverall@hotmail.co.uk>
2026-03-26 19:42:45 +0000

    reduced detrend_gap_threshold to 0.1
    Should help with LCs with discontinuities.

--- generate_lops2_config.py
+++ generate_lops2_config.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
         "target_config_defaults": {
             "quality_bitmask": None,        # leave choice to fit_lightcurves
             "quality_masks": [[1420.0, 1424.0], [1534.0, 1544.0]],
-            "detrend_gap_threshold": 0.5,
+            "detrend_gap_threshold": 0.1,
             "detrend_poly_degree": 2,
             "detrend_iterations": 3
         },
