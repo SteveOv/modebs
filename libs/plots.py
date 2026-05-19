922ff2ef5bd481c7a10376230ce381747dcbcf15
SteveOv <steveoverall@hotmail.co.uk>
2026-05-19 19:52:19 +0100

    fix plot_..._residuals failure if cols set to 1

--- libs/plots.py
+++ libs/plots.py
@@ -240,8 +240,8 @@ def plot_lightcurve_fits_and_residuals(out_files: List[Path],
                 ax_lc.invert_yaxis()
                 ax_res.invert_yaxis()
             else:
-                ax_lc.sharey(axes[0, 0])
-                ax_res.sharey(axes[1, 0])
+                ax_lc.sharey(axes.flat[0])
+                ax_res.sharey(axes.flat[1])
 
             # Hides tick labels on inner facing shared axes
             plt.setp(ax_lc.get_xticklabels(), visible=False)
